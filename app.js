7953e5caece1ab6e16982b5061aa8f7bed00e44d
Avishaan <avishaan@codehatcher.com>
2017-04-26 13:49:12 -0700

    updated to use true json file as input

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-var replace = require('replace-in-file');
+// var replace = require('replace-in-file');
+var fs = require('fs');
 var Chance = require('chance');
+var inputJSON = require('./input.json');
 
 var chance = new Chance();
 
-const options = {
 
-  //Single file
-  files: 'users.json',
-
-  //Replacement to make (string or regex)
-  replace: /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi,
-  with: chance.email(),
-
-  //Specify if empty/invalid file paths are allowed (defaults to false)
-  //If set to true these paths will fail silently and no error will be thrown.
-  allowEmptyPaths: false,
-
-  //Character encoding for reading/writing files (defaults to utf-8)
-  encoding: 'utf8',
-};
-
-replace(options, (error, changedFiles) => {
-  if (error) {
-    return console.error('Error occurred:', error);
-  }
-  console.log('Modified files:', changedFiles.join(', '));
+inputJSON.forEach(function(input){
+  input.emails[0].address = chance.email();
+  input.profile ? input.profile.user = chance.first() : null;
 });
+
+fs.writeFile('./output.json', JSON.stringify(inputJSON, null, 2));
